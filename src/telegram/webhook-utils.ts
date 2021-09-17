ce9f3739ecf1d9e7d08135efb5d6d28a14c4ca24
David Rolle <miridius@miridi.us>
2021-09-17 14:49:56 +0200

    Add sensible defaults to setWebhook

--- src/telegram/webhook-utils.ts
+++ src/telegram/webhook-utils.ts
@@ -4,8 +4,25 @@ import { callTgApi } from './telegram-api';
 const removeFalsyProps = <T extends object>(obj: T): Partial<T> =>
   Object.fromEntries(Object.entries(obj).filter(([, v]) => v)) as Partial<T>;
 
+export const defaultWebhookOpts: Partial<SetWebHookOptions> = {
+  // Process as many updates in parallel as the TG api will allow
+  max_connections: 100,
+  // These are all the types of updates that wrap-telegram will process
+  allowed_updates: [
+    'message',
+    'edited_message',
+    'channel_post',
+    'edited_channel_post',
+    'inline_query',
+  ],
+};
+
 export const setWebhook = async (opts: SetWebHookOptions) => {
-  opts = removeFalsyProps(opts) as SetWebHookOptions;
+  opts = {
+    ...defaultWebhookOpts,
+    ...removeFalsyProps(opts),
+  } as SetWebHookOptions;
+  if (!opts.url) throw new Error('You must provide a URL to set the webhook');
 
   console.debug('Setting webhook:', opts);
   await callTgApi({ method: 'setWebhook', ...opts });
