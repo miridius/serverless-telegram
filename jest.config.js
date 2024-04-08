32e35dd781d7541762793fc40b4a9079cd179263
David Rolle <david.rolle@griffin.sh>
2024-04-08 16:46:45 +0200

    Replace tsdx with esbuild

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 // https://jestjs.io/docs/en/configuration.html
 
 /** @type {import('@jest/types').Config.InitialOptions} */
-module.exports = {
+export default {
+  preset: 'ts-jest',
   // the code will run in node so we should also test in node (and it's faster)
   testEnvironment: 'node',
   // it's fastest to only use 1 worker (node startup is slower than the tests)
